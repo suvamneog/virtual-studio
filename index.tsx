0907d862f5d1f1d7f9fadbc63ef5a37e9ca8938d
Suvam Neog <162726362+suvamneog@users.noreply.github.com>
2025-10-08 23:33:57 +0530

    Updated index.tsx

--- index.tsx
+++ index.tsx
@@ -9,16 +9,8 @@ const Sign: HDKComponent<{ header: string; body: string; url: string }> = ({ ...
 
 const World = () => (
   <HNode>
-    <Ground />
     <Spawnpoint />
-    <Model src="music_studio_at_home.glb" z={5} />
-    <Model src="studio_monitor_jbl_lsr308.glb" x={2} z={5} />
-    <Sign
-      header="Welcome to Hiber3D HDK!"
-      body="This is The Getting Started world. Press O to learn how to build something!"
-      url="https://developer.hiber3d.com/docs/getting-started/fundamentals"
-      z={3}
-    />
+    <Model src="music_studio_at_home.glb" />
   </HNode>
 );
 
